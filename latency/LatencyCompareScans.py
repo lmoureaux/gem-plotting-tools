e3bfec42bbf16583e56f4890c67f79fdb3c9b620
Louis Moureaux <m_louis30@yahoo.com>
2017-07-06 16:42:09 +0200

    Add a legend to the comparison plots

--- latency/LatencyCompareScans.py
+++ latency/LatencyCompareScans.py
@@ -10,6 +10,9 @@ from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument(dest='inputs', nargs='+',
                     help='Files to compare', metavar='in.root')
+parser.add_argument('-l', '--legend', dest='legend',
+                    default=None, nargs='+',
+                    help='Legend entries', metavar='legend')
 parser.add_argument('-o', '--output', dest='output',
                     default='LatencyCompared',
                     help='Output file name without extension', metavar='out')
@@ -26,17 +29,28 @@ swatch = [ r.kBlue, r.kGreen, r.kMagenta, r.kCyan, r.kOrange,
 inputs = [ r.TFile(filename) for filename in options.inputs ]
 output = r.TFile(options.output + '.root', 'RECREATE')
 
+# Setup legend entries
+legendEntries = []
+if options.legend != None:
+  legendEntries = options.legend
+# Add defaults for missing entries
+for i in range(len(legendEntries), len(inputs)):
+  legendEntries.append(options.inputs[i])
+
 numInputs = len(inputs)
 for vfat in range(24):
   # Create the comparison
   canvas = r.TCanvas('canvas-lat-vfat%d' % vfat, '', 1000, 1000)
   canvas.cd()
+  legend = r.TLegend(0.7, 0.8, 0.9, 0.9)
   for i in range(numInputs):
     plot = inputs[i].Get('lat%d_ga' % vfat)
     plot.SetTitle('NHits(latency) for vfat %d' % vfat)
     plot.SetLineColor(swatch[i % len(swatch)])
     plot.GetYaxis().SetRangeUser(-0.05, 0.5)
     plot.Draw('' if i == 0 else 'same')
+    legend.AddEntry(plot, legendEntries[i])
+  legend.Draw()
   # Write to ROOT file
   output.cd()
   canvas.Write()
