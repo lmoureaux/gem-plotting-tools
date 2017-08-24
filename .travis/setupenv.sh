bbe34ecc268c8546370d60b223cd5e0d09b053b3
Louis Moureaux <lmoureau@ulb.ac.be>
2017-08-24 12:27:03 +0200

    Setup environment

---
+++ .travis/setupenv.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+# Script to setup data paths
+
+mkdir gemdata
+mkdir gemelog
+
+export BUILD_HOME="$TRAVIS_BUILD_DIR/.."
+export DATA_PATH="$BUILD_HOME/gemdata"
+export ELOG_PATH="$BUILD_HOME/gemelog"
+
+cd "$BUILD_HOME"
+
+echo "====== cmsgemos"
+. cmsgemos/setup/paths.sh
+echo "====== gem-plotting-tools"
+. gem-plotting-tools/setup/paths.sh
+
+# Finished
+echo "GEM DAQ Environment Configured"
