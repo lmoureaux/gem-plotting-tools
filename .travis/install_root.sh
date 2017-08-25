f3f3d69108eebc9da220559bc44def2a861e482e
Louis Moureaux <lmoureau@ulb.ac.be>
2017-08-25 16:01:04 +0200

    Don't source .travis/install_root.sh

--- .travis/install_root.sh
+++ .travis/install_root.sh
@@ -9,5 +9,3 @@ if [ ! -d root ]; then
     wget https://root.cern.ch/download/root_v5.34.36.Linux-ubuntu14-x86_64-gcc4.8.tar.gz
     tar -xzf root_v5.34.36.Linux-ubuntu14-x86_64-gcc4.8.tar.gz
 fi
-
-. root/bin/thisroot.sh
